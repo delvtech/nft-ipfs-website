709242c671c5483e46f23df68f640001e191620b
Matthew Brown <mattfdbrown@gmail.com>
2022-04-06 10:47:57 -1000

    update copy script

--- scripts/copy_files.ts
+++ scripts/copy_files.ts
@@ -1,8 +1,10 @@
 const fs = require("fs");
 
+console.log(__dirname);
+
 for (let i = 1; i <= 4700; i++) {
-  const filePath = `../metadatas/${i}.png`;
-  const filePathCopy = `../public/${i}.png`;
+  const filePath = `../metadatas/${i}`;
+  const filePathCopy = `../public/${i}`;
 
   fs.copyFile(filePath, filePathCopy, (err) => {
     if (err) throw err;
